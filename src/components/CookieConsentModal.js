d5526d9a5826c7022d6ccecc2d51b4f44c447026
agnes-cybil atsuah <agnescybil.atsuah@hyperisland.se>
2024-05-16 06:35:34 +0200

    Added responsive close button

--- src/components/CookieConsentModal.js
+++ src/components/CookieConsentModal.js
@@ -5,7 +5,7 @@ class CookieConsentModal extends React.Component {
   render() {
     return (
       <div className="modal">
-        <button class="close-button">X</button>
+               <button className="close-button" onClick={this.props.onClose}>X</button>
         <h2>This website utilises technologies such as cookies to enable essential site functionality, as well as for analytics, personalisation, and targeted advertising purposes.</h2>
         <p>You may change your settings at any time or accept the default settings. You may close this banner to continue with only essential cookies.</p>
        
@@ -33,9 +33,11 @@ class CookieConsentModal extends React.Component {
             </label>
           </div>
         </div>
-        <button onClick={this.props.onSave}>Save</button>
-        <button onClick={this.props.onAcceptAll}>Accept All</button>
-        <button onClick={this.props.onRejectAll}>Reject All</button>
+        <div className="button-container">
+  <button onClick={this.props.onSave}>Save</button>
+  <button onClick={this.props.onAcceptAll}>Accept All</button>
+  <button onClick={this.props.onRejectAll}>Reject All</button>
+</div>
       </div>
     );
   }
